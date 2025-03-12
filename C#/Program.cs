bae5bd383088a5595cf4c8557c34e0e995b79a6c
YeMyintAung259 <ymyintaung197@gmail.com>
2025-03-12 21:17:31 +0630

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+char character = 'a'; 
+Console.WriteLine(character);
+
+// Unicode character code in a hexadecimal format
+character = '\u003A'; 
+Console.WriteLine(character); 
+
+// Assigning the single quotiation character (escaped as \')
+character = '\''; 
+Console.WriteLine(character); 
+
+// Assigning the backslash character (escaped as \\)
+character = '\\'; 
+Console.WriteLine(character);
+string quotation = "\"Hello, Jude\", he said.";
+Console.WriteLine(quotation); 
+string path = "C:\\Windows\\Notepad.exe"; 
+Console.WriteLine(path); string verbatim = @"The \ is not escaped as \\. I am at a new line."; 
+Console.WriteLine(verbatim);
+
+
